3600551bf5ae075a3327469da12c133b1baf1082
kokkia-h <koki_aizawa@jp.honda>
2024-04-09 02:53:38 +0900

    [modify] save model and replay

--- src/experiments/irl_train_eval.py
+++ src/experiments/irl_train_eval.py
@@ -11,30 +11,33 @@ from src.experiments.base import BaseTrainer
 from src.misc.logger import Logger
 from src.misc.replay_buffer import get_replay_buffer, add_samples_to_replay_buffer
 from src.misc.video_recorder import VideoRecorder
+from src.misc.keras_utils import save_keras_model
 
-if tf.config.experimental.list_physical_devices('GPU'):
+if tf.config.experimental.list_physical_devices("GPU"):
     gpu = 0
     for cur_device in tf.config.experimental.list_physical_devices("GPU"):
         tf.config.experimental.set_memory_growth(cur_device, enable=True)
 
 
 class IRLTrainer(BaseTrainer):
-    def __init__(self,
-                 policy,
-                 imitator,
-                 env,
-                 eval_env,
-                 args,
-                 expert_states,
-                 expert_actions,
-                 expert_next_states,
-                 expert_dones,
-                 expert_logps,
-                 shift=None,
-                 scale=None,
-                 no_expert=False,
-                 buffer_size=None,
-                 use_init_states=False):
+    def __init__(
+        self,
+        policy,
+        imitator,
+        env,
+        eval_env,
+        args,
+        expert_states,
+        expert_actions,
+        expert_next_states,
+        expert_dones,
+        expert_logps,
+        shift=None,
+        scale=None,
+        no_expert=False,
+        buffer_size=None,
+        use_init_states=False,
+    ):
         self._set_from_args(args)
         self.env = env
         self.eval_env = eval_env
@@ -48,61 +51,56 @@ class IRLTrainer(BaseTrainer):
         if buffer_size is None:
             buffer_size = args.max_timesteps * 2
         self.replay_buffer = get_replay_buffer(env, size=buffer_size)
-        self.replay_buffer_iter = iter(
-            self.replay_buffer.as_dataset(
-                sample_batch_size=args.sample_batch_size))
+        self.replay_buffer_iter = iter(self.replay_buffer.as_dataset(sample_batch_size=args.sample_batch_size))
 
         self.policy_replay_buffer = get_replay_buffer(env, size=buffer_size)
         self.policy_replay_buffer_iter = iter(
-            self.policy_replay_buffer.as_dataset(
-                sample_batch_size=args.sample_batch_size))
+            self.policy_replay_buffer.as_dataset(sample_batch_size=args.sample_batch_size)
+        )
 
         self._use_init_states = use_init_states
         if use_init_states:
-            init_spec = tensor_spec.TensorSpec(
-                [env.observation_space.shape[0]], tf.float32, 'observation')
-            self.init_replay_buffer = get_replay_buffer(
-                env, size=args.max_timesteps, spec=init_spec)
+            init_spec = tensor_spec.TensorSpec([env.observation_space.shape[0]], tf.float32, "observation")
+            self.init_replay_buffer = get_replay_buffer(env, size=args.max_timesteps, spec=init_spec)
             self.init_replay_buffer_iter = iter(
-                self.init_replay_buffer.as_dataset(
-                    sample_batch_size=self._sample_batch_size))
+                self.init_replay_buffer.as_dataset(sample_batch_size=self._sample_batch_size)
+            )
 
         if not no_expert:
             expert_states = tf.Variable(expert_states, dtype=tf.float32)
             expert_actions = tf.Variable(expert_actions, dtype=tf.float32)
-            expert_next_states = tf.Variable(expert_next_states,
-                                             dtype=tf.float32)
+            expert_next_states = tf.Variable(expert_next_states, dtype=tf.float32)
             expert_dones = tf.Variable(expert_dones, dtype=tf.float32)
             expert_logps = tf.Variable(expert_logps, dtype=tf.float32)
 
-            expert_dataset = tf.data.Dataset.from_tensor_slices(
-                (expert_states, expert_actions, expert_next_states))
+            expert_dataset = tf.data.Dataset.from_tensor_slices((expert_states, expert_actions, expert_next_states))
 
-            self.expert_dataset = expert_dataset.repeat().shuffle(
-                expert_states.shape[0]).batch(self._sample_batch_size,
-                                              drop_remainder=True)
+            self.expert_dataset = (
+                expert_dataset.repeat()
+                .shuffle(expert_states.shape[0])
+                .batch(self._sample_batch_size, drop_remainder=True)
+            )
             self.expert_dataset_iter = iter(self.expert_dataset)
 
-        time_str = datetime.datetime.now().strftime('%Y%m%dT%H%M%S.%f')
+        time_str = datetime.datetime.now().strftime("%Y%m%dT%H%M%S.%f")
         self._output_dir = os.path.join(args.save_dir, time_str)
         if not os.path.exists(self._output_dir):
             os.makedirs(self._output_dir)
 
         # Create directory to save final models
         if self._save_model:
-            self._model_dir = os.path.join(self._output_dir, 'models')
+            self._model_dir = os.path.join(self._output_dir, "models")
             if not os.path.exists(self._model_dir):
                 os.makedirs(self._model_dir)
 
         # Set up loggers
         self.logger = Logger(self._output_dir, args, agent=args.algo)
-        self.video_recorder = VideoRecorder(
-            self._output_dir if self._save_test_movie else None)
+        self.video_recorder = VideoRecorder(self._output_dir if self._save_test_movie else None)
 
         # tenorboard
-        self.airl_log_dir = 'logs/airl'
+        self.airl_log_dir = "logs/airl"
         self.airl_writer = tf.summary.create_file_writer(self.airl_log_dir)
-        self.policy_log_dir = 'logs/policy'
+        self.policy_log_dir = "logs/policy"
         self.policy_writer = tf.summary.create_file_writer(self.policy_log_dir)
         self.total_steps = 0
         self.reward_log = 0
@@ -120,29 +118,31 @@ class IRLTrainer(BaseTrainer):
 
         while total_timesteps <= self._max_timesteps:
             if total_timesteps % self._eval_interval == 0:
-                logging.info('Performing policy eval.')
-                average_returns, evaluation_timesteps = self.evaluate(
-                    total_timesteps)
+                logging.info("Performing policy eval.")
+                average_returns, evaluation_timesteps = self.evaluate(total_timesteps)
 
                 eval_returns.append(average_returns)
 
-                self.logger.log('eval/average_test_return', average_returns)
-                self.logger.log('eval/average_test_episode_length',
-                                evaluation_timesteps)
-                self.logger.dump(total_timesteps,
-                                 dump_to_csv=True,
-                                 dump_eval=True)
-                logging.info('Eval: ave returns=%f, ave episode length=%f',
-                             average_returns, evaluation_timesteps)
+                self.logger.log("eval/average_test_return", average_returns)
+                self.logger.log("eval/average_test_episode_length", evaluation_timesteps)
+                self.logger.dump(total_timesteps, dump_to_csv=True, dump_eval=True)
+                logging.info("Eval: ave returns=%f, ave episode length=%f", average_returns, evaluation_timesteps)
+                if self._save_model:
+                    save_keras_model(
+                        self.imitator.rew_net, os.path.join(self._model_dir, "rew_net" + str(total_timesteps))
+                    )
+                    save_keras_model(
+                        self.policy.actor.trunk, os.path.join(self._model_dir, "actor_net" + str(total_timesteps))
+                    )
             if done:
                 if episode_timesteps > 0:
                     current_time = time.time()
                     fps = episode_timesteps / (current_time - previous_time)
-                    self.logger.log('common/training_return', episode_return)
-                    self.logger.log('common/fps', fps)
+                    self.logger.log("common/training_return", episode_return)
+                    self.logger.log("common/fps", fps)
                     if self._log_env_diagnostics:
                         for k, v in diagnostics.items():
-                            self.logger.log(f'common/{k}', v)
+                            self.logger.log(f"common/{k}", v)
 
                 obs = self.env.reset()
                 episode_return = 0
@@ -151,14 +151,11 @@ class IRLTrainer(BaseTrainer):
 
             if total_timesteps < self._num_random_actions:
                 action = self.env.action_space.sample()
-                logp = self.policy.get_logp(np.array([obs]),
-                                            np.array([action]))
+                logp = self.policy.get_logp(np.array([obs]), np.array([action]))
             else:
                 mean_action, _, logp = self.policy.actor.call(np.array([obs]))
                 action = mean_action[0].numpy()
-                action = (action +
-                          np.random.normal(0, 0.1, size=action.shape)).clip(
-                              -1, 1)
+                action = (action + np.random.normal(0, 0.1, size=action.shape)).clip(-1, 1)
             next_obs, reward, done, diagnostics = self.env.step(action)
             self.reward_log = reward
 
@@ -168,10 +165,8 @@ class IRLTrainer(BaseTrainer):
             if done and not truncated_done:
                 next_obs = self.env.get_absorbing_state()
 
-            add_samples_to_replay_buffer(self.replay_buffer, obs, action,
-                                         next_obs, logp)
-            add_samples_to_replay_buffer(self.policy_replay_buffer, obs,
-                                         action, next_obs, logp)
+            add_samples_to_replay_buffer(self.replay_buffer, obs, action, next_obs, logp)
+            add_samples_to_replay_buffer(self.policy_replay_buffer, obs, action, next_obs, logp)
             if done and not truncated_done:
                 # Add several absobrsing states to absorbing states transitions.
                 for abs_i in range(self._absorbing_per_episode):
@@ -179,14 +174,10 @@ class IRLTrainer(BaseTrainer):
                         obs = self.env.get_absorbing_state()
                         action = self.env.action_space.sample()
                         next_obs = self.env.get_absorbing_state()
-                        logp = self.policy.actor.get_log_prob(
-                            np.array([obs]), np.array([action]))
+                        logp = self.policy.actor.get_log_prob(np.array([obs]), np.array([action]))
 
-                        add_samples_to_replay_buffer(self.replay_buffer, obs,
-                                                     action, next_obs, logp)
-                        add_samples_to_replay_buffer(self.policy_replay_buffer,
-                                                     obs, action, next_obs,
-                                                     logp)
+                        add_samples_to_replay_buffer(self.replay_buffer, obs, action, next_obs, logp)
+                        add_samples_to_replay_buffer(self.policy_replay_buffer, obs, action, next_obs, logp)
 
             episode_return += reward
             episode_timesteps += 1
@@ -214,8 +205,7 @@ class IRLTrainer(BaseTrainer):
             self.video_recorder.init(enabled=(i == 0))
             done = False
             while not done:
-                action, _, _ = self.policy.actor(np.array([state]),
-                                                 training=False)
+                action, _, _ = self.policy.actor(np.array([state]), training=False)
                 action = action[0].numpy()
 
                 next_state, reward, done, _ = self.eval_env.step(action)
@@ -225,91 +215,83 @@ class IRLTrainer(BaseTrainer):
                 eval_total_timesteps += 1
                 state = next_state
             if self._save_test_movie and total_timesteps in self._save_movie_indices:
-                print('saving test movie')
-                self.video_recorder.save(f'{total_timesteps}.mp4',
-                                         logger=self.logger,
-                                         step=total_timesteps)
+                print("saving test movie")
+                self.video_recorder.save(f"{total_timesteps}.mp4", logger=self.logger, step=total_timesteps)
 
         return total_returns / num_episodes, eval_total_timesteps / num_episodes
 
     def _train_imitator(self):
         for _ in range(self._imitator_updates_per_step):
-            return_dict = self.imitator.update(
-                **self._get_imitator_train_kwargs())
+            return_dict = self.imitator.update(**self._get_imitator_train_kwargs())
 
-            self.logger.log('train/disc_loss', return_dict['train/gail_loss'])
+            self.logger.log("train/disc_loss", return_dict["train/gail_loss"])
         with self.airl_writer.as_default():
-            tf.summary.scalar('gail_classification_loss', return_dict['train/gail_classification_loss'], self.total_steps)
-            tf.summary.scalar('gail_gradient_penalty', return_dict['train/gail_gradient_penalty'], self.total_steps)
-            tf.summary.scalar('gail_loss', return_dict['train/gail_loss'], self.total_steps)
+            tf.summary.scalar(
+                "gail_classification_loss", return_dict["train/gail_classification_loss"], self.total_steps
+            )
+            tf.summary.scalar("gail_gradient_penalty", return_dict["train/gail_gradient_penalty"], self.total_steps)
+            tf.summary.scalar("gail_loss", return_dict["train/gail_loss"], self.total_steps)
 
     def _train_policy(self):
         for _ in range(self._policy_updates_per_step):
-            if self._algo == 'bc':
-                expert_states, expert_actions, expert_next_states = next(
-                    self.expert_dataset_iter)
-                return_dict = self.policy.train_bc(expert_states,
-                                                   expert_actions)
+            if self._algo == "bc":
+                expert_states, expert_actions, expert_next_states = next(self.expert_dataset_iter)
+                return_dict = self.policy.train_bc(expert_states, expert_actions)
             else:
                 kwargs = self._get_policy_train_kwargs()
                 return_dict = self.policy.train(**kwargs)
-                return_dict['train/imitator_reward'] = np.mean(
-                    kwargs['rewards'])
+                return_dict["train/imitator_reward"] = np.mean(kwargs["rewards"])
             self._log_to_logger(return_dict)
 
         with self.policy_writer.as_default():
-            tf.summary.scalar('env_reward', self.reward_log, self.total_steps)
-            tf.summary.scalar('critic_loss', return_dict['train/critic_loss'], self.total_steps)
-            tf.summary.scalar('actor_loss', return_dict['train/actor_loss'], self.total_steps)
-            tf.summary.scalar('bc_loss', return_dict['train/bc_loss'], self.total_steps)
-            tf.summary.scalar('alpha_loss', return_dict['train/alpha_loss'], self.total_steps)
-            tf.summary.scalar('actor_entropy', return_dict['train/actor_entropy'], self.total_steps)
-            tf.summary.scalar('alpha', return_dict['train/alpha'], self.total_steps)
-            tf.summary.scalar('imitation_reward', return_dict['train/imitator_reward'], self.total_steps)
+            tf.summary.scalar("env_reward", self.reward_log, self.total_steps)
+            tf.summary.scalar("critic_loss", return_dict["train/critic_loss"], self.total_steps)
+            tf.summary.scalar("actor_loss", return_dict["train/actor_loss"], self.total_steps)
+            tf.summary.scalar("bc_loss", return_dict["train/bc_loss"], self.total_steps)
+            tf.summary.scalar("alpha_loss", return_dict["train/alpha_loss"], self.total_steps)
+            tf.summary.scalar("actor_entropy", return_dict["train/actor_entropy"], self.total_steps)
+            tf.summary.scalar("alpha", return_dict["train/alpha"], self.total_steps)
+            tf.summary.scalar("imitation_reward", return_dict["train/imitator_reward"], self.total_steps)
 
     def _log_to_logger(self, return_dict):
         for k, v in return_dict.items():
             self.logger.log(k, v)
 
     def _get_policy_train_kwargs(self):
-        states, actions, next_states, _, masks, logp = next(
-            self.replay_buffer_iter)[0]
+        states, actions, next_states, _, masks, logp = next(self.replay_buffer_iter)[0]
 
-        rewards = self.imitator.predict_reward(states, actions, next_states,
-                                               logp)
+        rewards = self.imitator.predict_reward(states, actions, next_states, logp)
         kwargs = {
-            'states': states,
-            'actions': actions,
-            'next_states': next_states,
-            'rewards': rewards,
-            'masks': masks,
-            'target_entropy': -self.env.action_space.shape[0]
+            "states": states,
+            "actions": actions,
+            "next_states": next_states,
+            "rewards": rewards,
+            "masks": masks,
+            "target_entropy": -self.env.action_space.shape[0],
         }
 
         if self._use_init_states:
             init_states = next(self.init_replay_buffer)[0]
         else:
             init_states = states
-        kwargs['init_states'] = init_states
-        kwargs['actor_update_freq'] = self._actor_update_freq
+        kwargs["init_states"] = init_states
+        kwargs["actor_update_freq"] = self._actor_update_freq
 
         return kwargs
 
     def _get_imitator_train_kwargs(self):
-        expert_states, expert_actions, expert_next_states = next(
-            self.expert_dataset_iter)
-        policy_states, policy_actions, policy_next_states, _, _, _ = next(
-            self.policy_replay_buffer_iter)[0]
+        expert_states, expert_actions, expert_next_states = next(self.expert_dataset_iter)
+        policy_states, policy_actions, policy_next_states, _, _, _ = next(self.policy_replay_buffer_iter)[0]
 
         kwargs = {
-            'expert_states': expert_states,
-            'expert_actions': expert_actions,
-            'policy_states': policy_states,
-            'policy_actions': policy_actions
+            "expert_states": expert_states,
+            "expert_actions": expert_actions,
+            "policy_states": policy_states,
+            "policy_actions": policy_actions,
         }
 
-        kwargs['expert_next_states'] = expert_next_states
-        kwargs['policy_next_states'] = policy_next_states
-        kwargs['actor'] = self.policy.actor
+        kwargs["expert_next_states"] = expert_next_states
+        kwargs["policy_next_states"] = policy_next_states
+        kwargs["actor"] = self.policy.actor
 
         return kwargs
--- src/policies/gaussian_actor.py
+++ src/policies/gaussian_actor.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
+from src.misc.keras_utils import load_keras_model
 
 ds = tfp.distributions
 
@@ -11,34 +12,29 @@ EPS = 1e-6
 
 class Actor(tf.keras.Model):
     """Gaussian policy with TanH squashing."""
-    def __init__(self,
-                 state_dim,
-                 action_dim,
-                 loaded_actor=None,
-                 use_multivariate=False):
+
+    def __init__(self, state_dim, action_dim, loaded_actor=None, use_multivariate=False):
         """Creates an actor.
 
-    Args:
-      state_dim: State size.
-      action_dim: Action size.
-    """
+        Args:
+          state_dim: State size.
+          action_dim: Action size.
+        """
         super(Actor, self).__init__()
         self._use_multivariate = use_multivariate
         if loaded_actor is not None:
             self.trunk = loaded_actor
         else:
-            self.trunk = tf.keras.Sequential([
-                tf.keras.layers.Dense(256,
-                                      input_shape=(state_dim, ),
-                                      activation=tf.nn.relu,
-                                      kernel_initializer='orthogonal'),
-                tf.keras.layers.Dense(256,
-                                      activation=tf.nn.relu,
-                                      kernel_initializer='orthogonal'),
-                tf.keras.layers.Dense(2 * action_dim,
-                                      kernel_initializer='orthogonal')
-            ],
-                                             name='actor')
+            self.trunk = tf.keras.Sequential(
+                [
+                    tf.keras.layers.Dense(
+                        256, input_shape=(state_dim,), activation=tf.nn.relu, kernel_initializer="orthogonal"
+                    ),
+                    tf.keras.layers.Dense(256, activation=tf.nn.relu, kernel_initializer="orthogonal"),
+                    tf.keras.layers.Dense(2 * action_dim, kernel_initializer="orthogonal"),
+                ],
+                name="actor",
+            )
 
     def get_dist_and_mode(self, states):
         out = self.trunk(states)
@@ -47,24 +43,22 @@ class Actor(tf.keras.Model):
 
         log_std = tf.nn.tanh(log_std)
         assert LOG_STD_MAX > LOG_STD_MIN
-        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std +
-                                                                     1)
+        log_std = LOG_STD_MIN + 0.5 * (LOG_STD_MAX - LOG_STD_MIN) * (log_std + 1)
         std = tf.exp(log_std)
 
         if self._use_multivariate:
-            dist = tfp.distributions.MultivariateNormalDiag(loc=mu,
-                                                            scale_diag=std)
+            dist = tfp.distributions.MultivariateNormalDiag(loc=mu, scale_diag=std)
         else:
             dist = ds.TransformedDistribution(
-                ds.Sample(ds.Normal(tf.zeros(mu.shape[:-1]),
-                                    1.0,
-                                    validate_args=True),
-                          sample_shape=mu.shape[-1:]),
-                tfp.bijectors.Chain([
-                    tfp.bijectors.Tanh(validate_args=True),
-                    tfp.bijectors.Shift(shift=mu),
-                    tfp.bijectors.ScaleMatvecDiag(scale_diag=std)
-                ]))
+                ds.Sample(ds.Normal(tf.zeros(mu.shape[:-1]), 1.0, validate_args=True), sample_shape=mu.shape[-1:]),
+                tfp.bijectors.Chain(
+                    [
+                        tfp.bijectors.Tanh(validate_args=True),
+                        tfp.bijectors.Shift(shift=mu),
+                        tfp.bijectors.ScaleMatvecDiag(scale_diag=std),
+                    ]
+                ),
+            )
 
         return dist, mode
 
@@ -82,3 +76,6 @@ class Actor(tf.keras.Model):
         log_probs = dist.log_prob(samples)
         log_probs = tf.expand_dims(log_probs, -1)  # To avoid broadcasting
         return mode, samples, log_probs
+
+    def load_model(self, path):
+        self.trunk = load_keras_model(path)
